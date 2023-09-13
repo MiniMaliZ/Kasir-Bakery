0b352030fc64956cd67b67ba4238f536130b2640
MiniMaliZ <gakisomlebu@gmail.com>
2023-09-13 13:49:47 +0700

    First Dev

--- OurFirstGithubCollab.java
+++ OurFirstGithubCollab.java
@@ -1,5 +1,8 @@
 public class OurFirstGithubCollab{
     public static void main(String[] args) {
+        //Pertama
         System.out.println("Ini ceritanya fitur pertama yang sudah berjalan dengan baik");
+        //Kedua
+        System.out.println("Ketua Mulai mengembangkan fitur kedua");
     }
 }
